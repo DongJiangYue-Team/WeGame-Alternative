27379e0cebe07c8399f2f12be384fbae704de9da
DongJiangYue <39184341+DongJiangYue-Team@users.noreply.github.com>
2022-01-24 23:59:51 +0800

    Create LOL_Client_Download.bat

---
+++ aria2-batch/LOL_Client_Download.bat
@@ -0,0 +1,4 @@
+@pushd %~dp0 >nul 2>nul
+aria2\aria2c.exe --allow-overwrite="true" -d Resources https://cdn.jsdelivr.net/gh/DongJiangYue-Team/CN_LOL_DNF-Client-Patch-Download@main/aria2-config/LOL/Client-Latestversion/LOL_Client
+aria2\aria2c.exe aria2c --continue="true" --referer="http://down.qq.com/" --user-agent="Mozilla/4.0 (compatible; MSIE 9.11; Windows NT 10.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; .TDCH 7.0;)" -d Download -i Resources\LOL_Client
+exit
